61c293acc1ef6d090e3057f05e4cc308e9965c6f
deividy <deividyferreira.n@gmail.com>
2021-11-07 01:08:28 -0300

    Devugando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     return res.json({ message:'hello world' })
+})
+
+app.post('/courses', (req,res) => {
+    const { name } = req.body;
+    return res.json({ name })
+
 })
 
 app.listen(3333, () => console.log('listen on port: 3333'))
