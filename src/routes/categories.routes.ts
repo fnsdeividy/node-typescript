3d457b3e1c43d39e7fafb67ea43475dceeb86995
deividy <deividyferreira.n@gmail.com>
2021-11-07 02:02:59 -0300

    Validando o cadastro de categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -7,16 +7,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoute.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: "Category already exists !" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
 });
 
-categoriesRoute.get('/', (req, res) => {
-    const getAll = categoriesRepository.list()
+categoriesRoute.get("/", (req, res) => {
+  const getAll = categoriesRepository.list();
 
-    return res.json(getAll)
-
-})
+  return res.json(getAll);
+});
 
 export { categoriesRoute };
