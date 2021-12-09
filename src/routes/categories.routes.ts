cbfb258a49617b0ae4f5454acb31904d059d66b2
Deividy <fnsdeividy@gmail.com>
2021-12-09 17:46:38 -0300

    Criando upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
+import multer from "multer"
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoute = Router();
+
+const upload = multer({
+  dest:'./tmp'
+})
+  
 
 categoriesRoute.post("/", (request, response) => {
   return createCategoryController.handle(request, response)
@@ -11,5 +18,11 @@ categoriesRoute.post("/", (request, response) => {
 categoriesRoute.get("/", (request, response) => {
   return listCategoriesController.handle(request, response)
 });
+
+categoriesRoute.post('/import', upload.single('file'), (request, response) => {
+  const {file} = request
+  console.log(file)
+  return response.send()
+})
 
 export { categoriesRoute };
